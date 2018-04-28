80f462e81a99dc7f6d66218d994784bb1058f127
x24ken <x24ken@gmail.com>
2018-04-28 06:38:11 +0000

    chap3-2終わり

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const http = require('http');
 const fs = require('fs');
 const ejs = require('ejs');
 const url = require('url');
+const qs = require('querystring');
 
 const index_page = fs.readFileSync('./index.ejs', 'utf8');
 const other_page = fs.readFileSync('./other.ejs', 'utf8');
@@ -17,28 +18,16 @@ console.log('Server Start!');
 
 // createServerの処理
 function getFromClient(request, response){
-    var url_parts = url.parse(request.url);
+    var url_parts = url.parse(request.url, true);
     switch (url_parts.pathname) {
         
         case '/':
-            var content = ejs.render(index_page, {
-                title: "Index",
-                content: "これはテンプレートを使ったサンプルページです。"
-            });
-            response.writeHead(200, {'Content-Type': 'text/html'});
-            response.write(content);
-            response.end();
+            response_index(request, response);
             break;
             
-        case '/other':
-            var content = ejs.render(other_page, {
-                title: "Other",
-                content: "これは新しく用意したページです。"
-            });
-            response.writeHead(200, {'Content-Type': 'text/html'});
-            response.write(content);
-            response.end();
-            break;
+        // case '/other':
+        //     response_other(request, response);
+        //     break;
         
         case '/style.css':
             response.writeHead(200, {'Content-Type': 'text/css'});
@@ -52,3 +41,78 @@ function getFromClient(request, response){
             break;
     }
 }
+
+// 追加するデータ用変数
+var data = {msg:'no message...'}; 
+
+
+// indexのアクセス処理
+function response_index(request, response){
+    // POSTアクセス時の処理
+    if (request.method == 'POST'){
+        var body='';
+        
+        // データー受信のイベント処理
+        request.on('data', (data) => {
+            body +=data;
+        });
+        
+        // データ受信終了のイベント処理
+        request.on('end', () => {
+            data = qs.parse(body); // データのパス
+            //クッキーの保存
+            setCookie('msg', data.msg, response);
+            write_index(request, response);
+        });
+    } else {
+        write_index(request, response);
+    }
+}
+
+// indexの表示の作成
+function write_index(request, response) {
+    var msg = "※伝言を表示します。"
+    var cookie_data = getCookie('msg', request);
+    var content = ejs.render(index_page, {
+        title:"Index",
+        content:msg,
+        data:data,
+        cookie_data:cookie_data,
+    });
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write(content);
+    response.end();
+}
+
+// クッキーの値を設定
+function setCookie(key, value, response) {
+    var cookie = escape(value);
+    response.setHeader('Set-Cookie', [key + '=' + cookie]);
+}
+
+// クッキーの値を取得
+function getCookie(key, request) {
+    var cookie_data = request.headers.cookie != undefined ?
+        request.headers.cookie : '';
+    var data = cookie_data.split(';');
+    for(var i in data){
+        if (data[i].trim().startsWith(key + '=')){
+            var result = data[i].trim().substring(key.length + 1);
+            return unescape(result);
+        }
+    }
+    return '';
+}
+
+
+// function response_other(request, response){
+//     var msg = "これはOtherページです"
+//         var content = ejs.render(other_page, {
+//             title:"Other",
+//             content:msg,
+//             data:data2,
+//             filename: 'data_item'
+//         });
+//         response.writeHead(200, {'Content-Type': 'text/html'});
+//         response.write(content);
+// }
